e13657bbd082484fd261af55da64fca798d3fa6e
juanjts <1843@holbertonschool.com>
2020-06-11 20:55:14 +0000

    task0corr

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,16 +17,13 @@ int main(void)
 	{
 		printf("%d is negative \n", n);
 	}
+	else if (n > 0)
+	{
+		printf("%d is positive \n", n);
+	}
 	else
 	{
-		if (n > 0)
-		{
-			printf("%d is positive \n", n);
-		}
-		else
-		{
-			printf("%d is zero \n", n);
-		}
+		printf("%d is zero \n", n);
 	}
 	return (0);
 }
