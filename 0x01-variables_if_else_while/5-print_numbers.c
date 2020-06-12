a05bb76a64b8e18ebd030b1487935a70fd10b6c3
juanjts <1843@holbertonschool.com>
2020-06-12 00:12:13 +0000

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - entry block
+  * @void: no argument
+  * Return: 0
+**/
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+	printf("%d", b);
+	}
+	printf("\n");
+	return (0);
+}
