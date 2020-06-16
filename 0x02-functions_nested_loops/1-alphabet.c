5374ad1935d190140dec9501a0a2fc398b008dbd
juanjts <1843@holbertonschool.com>
2020-06-16 17:11:55 +0000

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/*
+ * main - Puts out 'Holberton' followed by a newline
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
