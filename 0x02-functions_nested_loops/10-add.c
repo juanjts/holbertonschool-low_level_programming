f4d348c4f01c72d1b8e7f5824da00a9459ecb511
juanjts <1843@holbertonschool.com>
2020-06-17 20:48:42 +0000

    task10correct

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - print 24 hours
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: last digit
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
