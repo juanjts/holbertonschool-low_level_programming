7e04911730d0d47e111ca9de255130fbe583bd5f
juanjts <1843@holbertonschool.com>
2020-06-17 02:08:18 +0000

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _islower- check c lowercase character
+ * @c: string
+ * Return: return 1 if c lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	return (0);
+}
