cf23e35e5ffc655773b0d758e4f7771e0311db66
juanjts <1843@holbertonschool.com>
2020-06-17 21:19:24 +0000

    task16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - is the function
+ * natural - Write a program that computes and prints the sum of
+ * all the multiples of 3 or 5 below 1024 (excluded)
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	float a, b, r;
+
+	a = 1;
+
+	b = 2;
+
+	r = 0;
+
+	printf("1, 2");
+
+	for (i = 1; i <= 96; i++)
+	{
+		r = a + b;
+		a = b;
+		b = r;
+		printf(", %.0f", r);
+	}
+	putchar('\n');
+	return (0);
+}
