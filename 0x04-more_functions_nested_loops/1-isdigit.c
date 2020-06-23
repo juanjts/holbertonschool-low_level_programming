9b21bdd3ea8a580dd54c8b38442dbc3553e273e1
juanjts <1843@holbertonschool.com>
2020-06-23 16:28:56 +0000

    task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isdigit - check the code for Holberton School students.
+ * @c: variable
+ * Return: 1 if c is uppercase and 0 if otherwise.
+ */
+int _isdigit(int c)
+{
+	for (c = 0; c <= 9; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
