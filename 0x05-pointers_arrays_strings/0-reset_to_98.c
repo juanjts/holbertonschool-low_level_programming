00142551ec0b90aa92f9784648b00a1222bce8c5
juanjts <1843@holbertonschool.com>
2020-06-25 15:35:55 +0000

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: is the pointer
+ * Return: if is need
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
