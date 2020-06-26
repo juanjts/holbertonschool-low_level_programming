83bc4f566e8c165399bddd1fcdc333c18adcb48c
juanjts <1843@holbertonschool.com>
2020-06-26 05:17:12 +0000

    task6changed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,15 @@
  */
 void puts2(char *str)
 {
-	int c;
+	int c = 0;
 
-	for (c = 0; str[c] != '\0' ; c += 2)
+	while (str[c] != '\0')
 	{
-		_putchar(str[c]);
+		if (c % 2 == 0)
+		{
+			_putchar(str[c]);
+		}
+		c++;
 	}
 	_putchar('\n');
 }
