b4f1a1ec44e2ba15412c001a434c5f391d846c35
juanjts <1843@holbertonschool.com>
2020-06-27 20:14:57 +0000

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Function that reverses the content of an array of integers.
+ * @a: is the array
+ * @n: is the number of array elements
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, Aux;
+
+	n = n - 1;
+	while (i <= n)
+	{
+		Aux = a[i];
+		a[i++] = a[n];
+		a[n--] = Aux;
+	}
+}
