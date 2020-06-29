7ab4f09120ab45364e2277768df813b625692ea1
juanjts <1843@holbertonschool.com>
2020-06-29 02:29:28 +0000

    task7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints buffer in hexa
+ * @a: para pointer
+ * @size: size of matrix
+ * Return: if is necesary
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, s = 0, t = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				s = s + a[(size + 1) * i];
+			}
+			if (i + j == size - 1)
+			{
+				t = t + a[(size - 1) * (i + 1)];
+			}
+		}
+	}
+	printf("%i, %i\n", s, t);
+}
