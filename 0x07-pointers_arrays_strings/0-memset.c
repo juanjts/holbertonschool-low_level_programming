32524cc948c3aa6d6455370552168be40e58c05f
juanjts <1843@holbertonschool.com>
2020-06-29 01:18:06 +0000

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _memset - Fill n bytes of memory with a constant byte
+ * @s: The memory area to fill
+ * @b: The constant byte
+ * @n: The number of bytes to fill
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
