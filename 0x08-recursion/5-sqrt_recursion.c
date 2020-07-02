b0002b41c63db34e683724c40c303085a5afe14a
juanjts <1843@holbertonschool.com>
2020-07-02 04:16:30 +0000

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * sqrt_a - check the code for Holberton School students.
+ * @a: static parameter
+ * @b: one by one parameter
+ * Return: dual parameter function
+ */
+
+int sqrt_dual(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+	return (sqrt_dual(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - check the code for Holberton School students.
+ * @n: number
+ * Return: function of function.
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_dual(n, 0));
+}
