5d5b8009cca2b46cf4c0bdfc2970f3da0c01477e
juanjts <1843@holbertonschool.com>
2020-07-05 23:39:50 +0000

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - ppal function
+ * @argc: counter of parameters
+ * @argv: value of parameters in especific memori position
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
