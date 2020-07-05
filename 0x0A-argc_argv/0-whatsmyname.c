123a4385513c99f8be9f736fccf5dc13f4bda2b7
juanjts <1843@holbertonschool.com>
2020-07-05 23:02:06 +0000

    task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "holberton.h"
 
-/*
-* main - ppal function
-* @argc: counter of parameters
-* @argv: value of parameters in especific memori position
-* return: zero
-*/
+/**
+ * main - ppal function
+ * @argc: counter of parameters
+ * @argv: value of parameters in especific memori position
+ * Return: zero
+ */
 int main(int argc, char *argv[])
 {
 	if (argc)
 	{
 		printf("%s \n", argv[0]);
 	}
-	return 0;
+	return (0);
 }
