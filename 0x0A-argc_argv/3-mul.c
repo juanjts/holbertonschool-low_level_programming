4ab547497cfb32ddafad2cb0c77394aa157f8a74
juanjts <1843@holbertonschool.com>
2020-07-06 01:37:41 +0000

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - ppal function
+ * @argc: counter of parameters
+ * @argv: value of parameters in especific memori position
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argc < 3)                                                                                                                                                           {
+		printf("ERROR\n");
+		return (1);
+	}
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+	printf("%d\n", result);
+	return (0);
+}
