f0b61e8a812a140b2b5ef518340eb70872fb6b42
juanjts <1843@holbertonschool.com>
2020-07-15 01:35:12 +0000

    newchang

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * new_dog - new dog
@@ -11,27 +12,23 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
-
 	int n, c, i;
 
 	for (n = 0; name[n] != '\0'; n++)
-	;
+		;
 	for (c = 0; owner[c] != '\0'; c++)
-	;
-
+		;
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-
 	new->name = malloc(sizeof(char) * (n + 1));
 	if (new->name == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-
 	new->owner = malloc(sizeof(char) * (c + 1));
 	if (new->owner == NULL)
 	{
@@ -39,13 +36,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new->name);
 		return (NULL);
 	}
-
 	for (i = 0; i <= n; i++)
 	{
 		new->name[i] = name[i];
 	}
 	new->age = age;
-
 	for (i = 0; i <= c; i++)
 	{
 		new->owner[i] = owner[i];
