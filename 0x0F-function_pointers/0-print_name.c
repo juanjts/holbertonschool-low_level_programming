57e79715a186711cd99d7b866fe6a09c29dec87c
juanjts <1840@holbertonschool.com>
2020-07-21 22:23:29 +0000

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - send a parameter to the functionpointer
+ * @name: name of the person
+ * @f: function painter
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
