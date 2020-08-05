82d5fc84f8d3307a0824dd56b3df77a9a70e3407
juanjts <1843@holbertonschool.com>
2020-08-05 17:23:20 +0000

    betty and code changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,8 +3,7 @@
  * main - copies the content of a file to another file
  * @argc: number of arguments passed to the program
  * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
@@ -12,25 +11,20 @@ int main(int argc, char *argv[])
 	char buf[BUFSIZ];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		if (fd_w < 0 || write(fd_w, buf , r) != r)
+		if (fd_w < 0 || write(fd_w, buf, r) != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_r);
-			exit(99);
+			close(fd_r), exit(99);
 		}
 	}
 	if (r < 0)
@@ -38,8 +32,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	a = close(fd_r);
-	b = close(fd_w);
+	a = close(fd_r), b = close(fd_w);
 	if (a < 0 || b < 0)
 	{
 		if (a < 0)
