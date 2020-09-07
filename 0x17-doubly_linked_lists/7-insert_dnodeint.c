34b964fc3a4ca4fac631174fc928a4e981baa3b6
juanjts <1843@holbertonschool.com>
2020-09-07 00:26:50 +0000

    changed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,22 +10,32 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = *h;
-	dlistint_t *new;
-	unsigned int i = 1;
+	dlistint_t *tmp = *h, *new;
+	unsigned int i = 0;
 
+	if (tmp == NULL || idx == 0)
+		return (add_dnodeint(h, n));
+	if (tmp->next == NULL && idx == 1)
+		return (add_dnodeint_end(h, n));
+	if (tmp->next == NULL && idx > 1)
+		return (NULL);
+	while (idx != 0)
+	{
+		tmp = tmp->next;
+		idx--;
+		i = idx;
+		if (tmp->next == NULL && (idx - 1) == 0)
+			return (add_dnodeint_end(h, n));
+		if (tmp->next == NULL && (i - 1) > 0)
+			return (NULL);
+	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	while (idx > i)
-	{
-		tmp = tmp->next;
-		i++;
-	}
-	new->next = tmp->next;
-	new->prev = tmp;
-	tmp->next->prev = new;
-	tmp->next = new;
-	return (tmp);
+	tmp->prev->next = new;
+	new->next = tmp;
+	new->prev = tmp->prev;
+	tmp->prev = new;
+	return (new);
 }
